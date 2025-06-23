9532a9412ae2424eee05e9afa62cc3c0f32d568f
Corine KOUASSI <ckouassi@quantech.solutions>
2025-06-23 03:08:23 +0000

    delete useless lines.

--- src/main/java/com/technical/evaluation/orders/features/inventaire/controller/InventaireController.java
+++ src/main/java/com/technical/evaluation/orders/features/inventaire/controller/InventaireController.java
@@ -1,6 +1,5 @@
 package com.technical.evaluation.orders.features.inventaire.controller;
 
-import com.technical.evaluation.orders.features.commande.dto.HistoriqueVenteProduitDto;
 import com.technical.evaluation.orders.features.inventaire.dto.InventaireResumeDto;
 import com.technical.evaluation.orders.features.inventaire.dto.RapportVenteDto;
 import com.technical.evaluation.orders.features.inventaire.service.InventaireService;
@@ -9,11 +8,8 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.UUID;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
